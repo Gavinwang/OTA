0d851a968707f6139e62510191629e6daa284e34
Gavinwang <lywang@teamsourcing.com.cn>
2009-09-04 11:22:01 +0800

    update readme

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 15) do
+ActiveRecord::Schema.define(:version => 14) do
 
   create_table "academics", :force => true do |t|
     t.string   "institution"
